757c0f738faa2e8856c039194a464cb829147e74
Priyal <111431040+Priyal340@users.noreply.github.com>
2023-05-02 08:52:16 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Logo from './logo.svg';
+import './App.css';
+import Todo from './component/Todo';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <Todo/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/component/Todo.js
@@ -0,0 +1,67 @@
+import React, {useEffect, useState} from 'react';
+import CreateTaskPopup from '../model/CreateTask';
+import Card from './Card';
+
+const Todo = () => 
+{
+    const [modal, setModal] = useState(false);
+    const [taskList, setTaskList] = useState([])
+
+    useEffect(() => 
+    {
+        let arr =localStorage.getItem("taskList")
+        
+        if(arr)
+        {
+            let obj = JSON.parse(arr)
+            setTaskList(obj)
+        }
+    }, [])
+
+    const deleteTask = (index) =>
+    {
+        let tempList = taskList
+        tempList.splice(index, 1)
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const updateListArray = (obj, index) =>
+    {
+        let tempList = taskList
+        tempList[index] = obj
+        localStorage.setItem("taskList", JSON.stringify(tempList))
+        setTaskList(tempList)
+        window.location.reload()
+    }
+
+    const toggle = () => 
+    {
+        setModal(!modal);
+    }
+
+    const saveTask = (taskObj) =>
+    {
+        let tempList = taskList
+        tempList.push(taskObj)
+        localStorage.setItem("taskList",JSON.stringify(tempList))
+        setTaskList(tempList)
+        setModal(false)
+    }
+
+    return (
+        <>
+            <div class="header text-center">
+                <h3>Todo List</h3>
+                <button class="btn btn-primary mt-2" onClick={()=> setModal(true)}>Create Task</button>
+            </div>
+            <div class="task-container">
+                {taskList.map((obj, index) => <Card taskObj={obj} index = {index} deleteTask = {deleteTask} updateListArray={updateListArray}/>)}
+            </div>
+            <CreateTaskPopup toggle={toggle} modal={modal} save = {saveTask} />
+        </>
+    );
+};
+
+export default Todo;
